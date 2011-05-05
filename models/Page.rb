7e664b99ed416ef2f54e9ce2fd47ab365ee9fae7
Eric Kelly <heroiceric@gmail.com>
2011-05-05 05:30:36 -0400

    first commit

---
+++ models/Page.rb
@@ -0,0 +1,22 @@
+class Page
+	include DataMapper::Resource
+	
+	property :id,         Serial
+	property :title,      String
+	property :slug,       String
+  property :keywords,   String, :length => 1024
+	property :body,       Text
+	property :created_at, DateTime
+	property :updated_at, DateTime
+	
+	has n, :comments
+	
+	def url
+		"/#{slug}"
+	end
+
+  def self.make_slug(title)
+		title.downcase.gsub(/ /, '_').gsub(/[^a-z0-9_]/, '').squeeze('_')
+	end
+
+end
